7f41957872e85d68880c3f341c6501f3da9088b8
Rob Windsor <rob.windsor@gmail.com>
2022-05-04 09:44:58 -0400

    FY22Q4 refresh
    - run through all exercise steps in associated MSLearn module to validate good working order
    - updated steps & fixed typos where necessary
    - implement changes per Acrolinx feedback
    - any & all users/names/company names/pictures/IDs are not real people/tenants... all are from M365 demo tenants provided by Microsoft
    - if applicable, port usage of ROPC to more secure option that also supports MFA

--- README.md
+++ README.md
@@ -42,6 +42,7 @@ This module has been recorded and is available in the Office Development YouTube
 
 | Version |        Date        |                                        Comments                                         |
 | ------- | ------------------ | --------------------------------------------------------------------------------------- |
+| 1.13    | May 17, 2022       | FY2022Q4 content refresh                                                                |
 | 1.12    | March 1, 2022      | FY2022Q3 content refresh                                                                |
 | 1.11    | December 8, 2021   | FY2022Q2 content refresh                                                                |
 | 1.10    | September 13, 2021 | FY2022Q1 content refresh                                                                |
--- demos/02-avoid-throttline-implement-strategies/part-02-graphclient/Program.cs
+++ demos/02-avoid-throttline-implement-strategies/part-02-graphclient/Program.cs
@@ -11,8 +11,8 @@ using System.Threading.Tasks;
 using Microsoft.Identity.Client;
 using Microsoft.Graph;
 using Microsoft.Extensions.Configuration;
-using Helpers;
 using System.Text.Json;
+using Helpers;
 
 namespace graphconsoleapp
 {
@@ -20,8 +20,6 @@ namespace graphconsoleapp
   {
     public static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
-
       var config = LoadAppSettings();
       if (config == null)
       {
@@ -29,10 +27,10 @@ namespace graphconsoleapp
         return;
       }
 
-      var userName = ReadUsername();
-      var userPassword = ReadPassword();
+      var client = GetAuthenticatedGraphClient(config);
 
-      var client = GetAuthenticatedGraphClient(config, userName, userPassword);
+      var profileResponse = client.Me.Request().GetAsync().Result;
+      Console.WriteLine("Hello " + profileResponse.DisplayName);
 
       var stopwatch = new System.Diagnostics.Stopwatch();
       stopwatch.Start();
@@ -99,52 +97,26 @@ namespace graphconsoleapp
       }
     }
 
-    private static IAuthenticationProvider CreateAuthorizationProvider(IConfigurationRoot config, string userName, SecureString userPassword)
+    private static IAuthenticationProvider CreateAuthorizationProvider(IConfigurationRoot config)
     {
       var clientId = config["applicationId"];
       var authority = $"https://login.microsoftonline.com/{config["tenantId"]}/v2.0";
 
       List<string> scopes = new List<string>();
-      scopes.Add("User.Read");
-      scopes.Add("Mail.Read");
+      scopes.Add("https://graph.microsoft.com/.default");
 
       var cca = PublicClientApplicationBuilder.Create(clientId)
                                               .WithAuthority(authority)
+                                              .WithDefaultRedirectUri()
                                               .Build();
-      return MsalAuthenticationProvider.GetInstance(cca, scopes.ToArray(), userName, userPassword);
+      return MsalAuthenticationProvider.GetInstance(cca, scopes.ToArray());
     }
 
-    private static GraphServiceClient GetAuthenticatedGraphClient(IConfigurationRoot config, string userName, SecureString userPassword)
+    private static GraphServiceClient GetAuthenticatedGraphClient(IConfigurationRoot config)
     {
-      var authenticationProvider = CreateAuthorizationProvider(config, userName, userPassword);
+      var authenticationProvider = CreateAuthorizationProvider(config);
       var graphClient = new GraphServiceClient(authenticationProvider);
       return graphClient;
-    }
-
-    private static SecureString ReadPassword()
-    {
-      Console.WriteLine("Enter your password");
-      SecureString password = new SecureString();
-      while (true)
-      {
-        ConsoleKeyInfo c = Console.ReadKey(true);
-        if (c.Key == ConsoleKey.Enter)
-        {
-          break;
-        }
-        password.AppendChar(c.KeyChar);
-        Console.Write("*");
-      }
-      Console.WriteLine();
-      return password;
-    }
-
-    private static string ReadUsername()
-    {
-      string? username;
-      Console.WriteLine("Enter your username");
-      username = Console.ReadLine();
-      return username ?? "";
     }
 
     private static Message GetMessageDetail(GraphServiceClient client, string messageId)
